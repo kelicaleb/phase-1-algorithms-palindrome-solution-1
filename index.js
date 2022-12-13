d9af655e31c5247aeea68f1dba315dc01176a10f
kelicaleb <kelicaleb7@gmail.com>
2022-12-13 21:07:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split('').reverse().join('') === word ? true : false
 }
 
 /* 
   Add your pseudocode here
+  Reverse string
+  if the reversed string equal to the original string
+    return true
+  otherwise
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  To get the reversed string, split the string to array of letter, reverse the array of letter and then rejoin the letter to a string again
 */
 
 // You can run `node index.js` to view these console logs
